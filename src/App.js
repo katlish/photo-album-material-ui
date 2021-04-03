96a4a8fd6d4ae684cd0a41c973c47be78c8739e0
shitashikat <radistkakat89@gmail.com>
2021-04-03 19:26:14 +0300

    first and final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container
+} from "@material-ui/core";
+import PhotoCameraIcon from "@material-ui/icons/PhotoCamera";
+import useStyles from './styles';
 
-function App() {
+const App = () => {
+  const classes = useStyles();
+  const cards = [1,2,3,4,5,6,7,8,9];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCameraIcon className={classes.icon}/>
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>Photo Album</Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>Hello guys this is a Album with super long description and i made it in purpose to test the typography attributes!!!!</Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">See My Photos</Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">Secondary Action</Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {
+              cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia 
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image Title"/>
+                    <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant="h5">
+                        Heading
+                      </Typography>
+                      <Typography>
+                        This is a media card. With this description. Blaaaaaaaaaaaaa
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" color="primary">View</Button>
+                      <Button size="small" color="primary">Edit</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))
+            }
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          Something here to give the footer a purpose!
+        </Typography>
+      </footer>
+    </>
   );
-}
+};
 
 export default App;
